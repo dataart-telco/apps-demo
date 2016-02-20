027503223317b53001e8c91d5218df2f9041c12f
Anton Chikin <kverlin@gmail.com>
2016-02-20 19:52:40 +0300

    opencell: added opencell api

--- opencell-billing/listener.go
+++ opencell-billing/listener.go
@@ -4,16 +4,26 @@ import (
 	"gopkg.in/redis.v3"
 	"tad-demo/common"
 	"tad-demo/common/opencell-api"
+	"time"
+	"fmt"
+)
+
+const (
+	opencellUser = "DA conference"
 )
 var cfg = common.NewConfig()
 var db = common.NewDbClient(cfg.Service.Redis)
 var restcommApi = common.NewRestcommApi(cfg.Service.Restcomm, cfg.Auth.User, cfg.Auth.Pass)
+var opencellApi = opencell_api.NewOpencellAPI(cfg.Opencell.Password, cfg.Opencell.Host)
+
 
 type BillingListener struct {
 }
 
 func (l BillingListener) Subscribe() {
 	//subscribe for events from call_status queue
+	opencellApi.InitOpenCell()
+	opencellApi.CreateNewCustomer(opencellUser)
 	go func() {
 		sub, _ := db.Subscribe(common.CHANNEL_CALL_STATUS)
 		for {
@@ -30,25 +40,18 @@ func (l BillingListener) Subscribe() {
 					if err != nil {
 						common.Error.Println("opencell-billing: Failed to query restcomm for call sid:%s", callStatus.CallSid)
 					} else {
-						time := callInfo.Duration
-						timestamp = 
-
+						duration := callInfo.Duration
+						timestamp := time.Now()
+						timestring := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d.000Z",
+							timestamp.Year(),
+							timestamp.Month(),
+							timestamp.Day(),
+							timestamp.Hour(),
+							timestamp.Minute(),
+							timestamp.Second())
+						opencellApi.ChargeCustomer(opencellUser, timestring, duration/60 )
 					}
 				}
-			}
-		}
-	}()
-	go func() {
-		sub, _ := db.Subscribe(common.CHANNEL_CONF_DROPPED)
-		for {
-			msg, e2 := sub.Receive()
-			if e2 != nil {
-				common.Error.Println("opencell-billing: Error receiving message from Redis 'call dropped' queue")
-				panic(e2)
-			}
-			switch v := msg.(type) {
-			case *redis.Message:
-				//do somethin on call drop
 			}
 		}
 	}()
