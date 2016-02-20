1845a3ab3b0ad4209fb8aab2eb92cbd3fd974d5c
Gennadiy Dubina <hamsterksu@gmail.com>
2016-02-20 19:46:04 +0200

    error handling, fix main

--- common/opencell-api/http_utils.go
+++ common/opencell-api/http_utils.go
@@ -16,32 +16,36 @@ func NewHttpUtils(basicAuthStr string) HttpUtils {
 	return HttpUtils{BasicAuthString: basicAuthStr}
 }
 
-func (this HttpUtils) DoPostSoap(url string, xml string) (int, error, http.Response) {
+func (this HttpUtils) DoPostSoap(url string, xml string) (int, error, *http.Response) {
 	
 	request, err := http.NewRequest("POST", url, bytes.NewBufferString(xml))
 	
 	if err != nil {
-		panic(err)
+		return 0, err, nil
 	}
 	
 	request.Header.Add("Authorization", this.BasicAuthString)
 
 	client := &http.Client{}
 	resp, err := client.Do(request)
+
+	if err != nil {
+		return 0, err, nil
+	}
 	
 	respBody, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(respBody))
 	
 	if err != nil {
 		fmt.Println(err)
-		return resp.StatusCode, err, *resp
+		return resp.StatusCode, err, resp
 	}
 
 	if resp.StatusCode != 200 {
-		return resp.StatusCode, errors.New("Resp code is not 200 for " + url + "; status = " + resp.Status), *resp
+		return resp.StatusCode, errors.New("Resp code is not 200 for " + url + "; status = " + resp.Status), resp
 	}
 	
-	return 0, err, *resp
+	return 0, err, resp
 }
 
 func (this HttpUtils) DoPostJson(url string, jsonData string) (int, error, http.Response) {
--- opencell-billing/listener.go
+++ opencell-billing/listener.go
@@ -24,35 +24,40 @@ func (l BillingListener) Subscribe() {
 	//subscribe for events from call_status queue
 	opencellApi.InitOpenCell()
 	opencellApi.CreateNewCustomer(opencellUser)
-	go func() {
-		sub, _ := db.Subscribe(common.CHANNEL_CALL_STATUS)
-		for {
-			msg, e2 := sub.Receive()
-			if e2 != nil {
-				common.Error.Println("opencell-billing: Error receiving message from Redis 'call_status' queue")
-				panic(e2)
-			}
-			switch v := msg.(type) {
-			case *redis.Message:
-				callStatus := common.NewCallStatus(v.Payload)
-				if callStatus.CallStatus == common.CallStatusCompleted {
-					callInfo, err := restcommApi.GetCallInfo(callStatus.CallSid)
-					if err != nil {
-						common.Error.Println("opencell-billing: Failed to query restcomm for call sid:%s", callStatus.CallSid)
-					} else {
-						duration := callInfo.Duration
-						timestamp := time.Now()
-						timestring := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d.000Z",
-							timestamp.Year(),
-							timestamp.Month(),
-							timestamp.Day(),
-							timestamp.Hour(),
-							timestamp.Minute(),
-							timestamp.Second())
-						opencellApi.ChargeCustomer(opencellUser, timestring, duration/60 )
-					}
-				}
-			}
+	
+	sub, _ := db.Subscribe(common.CHANNEL_CALL_STATUS)
+	for {
+		msg, e2 := sub.Receive()
+		if e2 != nil {
+			common.Error.Println("opencell-billing: Error receiving message from Redis 'call_status' queue")
+			panic(e2)
 		}
-	}()
+		switch v := msg.(type) {
+		case *redis.Message:
+			go func(){
+				l.DoMessage(v)
+			}()
+		}
+	}
+}
+
+func (l BillingListener) DoMessage(v *redis.Message) {
+	callStatus := common.NewCallStatus(v.Payload)
+	if callStatus.CallStatus == common.CallStatusCompleted {
+		callInfo, err := restcommApi.GetCallInfo(callStatus.CallSid)
+		if err != nil {
+			common.Error.Println("opencell-billing: Failed to query restcomm for call sid:%s", callStatus.CallSid)
+		} else {
+			duration := callInfo.Duration
+			timestamp := time.Now()
+			timestring := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d.000Z",
+				timestamp.Year(),
+				timestamp.Month(),
+				timestamp.Day(),
+				timestamp.Hour(),
+				timestamp.Minute(),
+				timestamp.Second())
+			opencellApi.ChargeCustomer(opencellUser, timestring, duration/60 )
+		}
+	}
 }
